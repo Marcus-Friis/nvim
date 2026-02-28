58ec628d5c93606cadb613318f32c0d3f4055258
Marcus Friis <marcus.friis99@gmail.com>
2026-02-28 13:29:03 +0100

    moved gitsigns to own file

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,17 +5,5 @@ return {
     config = function()
       require('nvim-highlight-colors').setup({})
     end
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
   }
 }
