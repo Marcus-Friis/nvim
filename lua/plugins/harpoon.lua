403a1dae39adcb05fd155969c384ff6d05622aff
Marcus Friis <marcus.friis99@gmail.com>
2026-02-28 03:10:37 +0100

    better harpoon bindings that do not clash with telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,8 +8,17 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<M-1>", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<M-2>", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<M-3>", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<M-4>", function()
+            harpoon:list():select(4)
+        end)
     end,
 }
