5a7ff6acf05f8ca19ecd5f6a17eb1e120f477c28
Marcus Friis <marcus.friis99@gmail.com>
2026-02-28 14:37:35 +0100

    Added mason tool installer for installing formatters

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,64 +1,73 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "mason-org/mason.nvim",
-        "mason-org/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "j-hui/fidget.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"j-hui/fidget.nvim",
+	},
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "ty",
-                "lua_ls",
-                "vtsls",
-                "eslint",
-                "tailwindcss",
-                "svelte",
-                "html",
-                "cssls",
-                "ruff",
-                "marksman",
-                "jsonls",
-                "yamlls"
-            },
-        })
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ty",
+				"lua_ls",
+				"vtsls",
+				"eslint",
+				"tailwindcss",
+				"svelte",
+				"html",
+				"cssls",
+				"ruff",
+				"marksman",
+				"jsonls",
+				"yamlls",
+			},
+		})
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+			},
+		})
 
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-                -- extra danish bindings
-                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = "copilot", group_index = 2 },
-                { name = 'nvim_lsp' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
-    end
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+
+				-- extra danish bindings
+				["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "copilot", group_index = 2 },
+				{ name = "nvim_lsp" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+	end,
 }
